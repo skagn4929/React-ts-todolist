e09983913801df6250fc8ca5b565e1e186921440
nohsangwoo <nsgr12@naver.com>
2023-11-09 00:25:28 +0900

    feat: 피드백v_1

--- src/components/TodoBoard.tsx
+++ src/components/TodoBoard.tsx
@@ -8,16 +8,41 @@ type TodoBoardProps = {
 const TodoBoard: React.FC<TodoBoardProps> = ({ todoList }) => {
   const [todos, setTodos] = useState(todoList);
   const [inputValue, setInputValue] = useState("");
+  const [updateValue, setUpdateValue] = useState("")
+  const [showUpdateButtonTrigger, setShowUpdateButtonTrigger] = useState(false)
   const handleAdd = () => {
     if (inputValue.trim() !== "") {
       setTodos([...todos, inputValue]);
       setInputValue("");
     }
   };
-  const handleDelete = (index: number) => {
-    const newTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
-    setTodos(newTodos);
+  const handleDelete = (index: number) => { //1
+    // #1
+    // const newTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
+    // setTodos(newTodos)
+
+    // #2
+    const newTodos = todos.filter((_, innerIndex) => innerIndex !== index)
+    setTodos(newTodos)
+
+    // #3
+    // setTodos((prev) => prev.filter((_, innerIndex) => innerIndex !== index));
   };
+
+
+  const handleUpdate = (index: number) => {
+    const newTodos = todos.map((todo, innerIndex) => { // 0,1,2    2 === 1
+      if (innerIndex === index) {
+        return updateValue
+      }
+      return todo
+    })
+
+    // console.log("newTodos: ", newTodos)
+    setTodos(newTodos)
+    setUpdateValue("")
+  }
+
   return (
     <>
       <div className="todoboard">
@@ -34,6 +59,9 @@ const TodoBoard: React.FC<TodoBoardProps> = ({ todoList }) => {
             key={index}
             item={todo}
             onDelete={() => handleDelete(index)}
+            updateValue={updateValue}
+            setUpdateValue={setUpdateValue}
+            handleUpdate={() => handleUpdate(index)}
           />
         ))}
       </div>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 type TodoItemProps = {
   item: string;
   onDelete: () => void;
+  handleUpdate: () => void;
+  updateValue: string
+  setUpdateValue: React.Dispatch<React.SetStateAction<string>>
 };
-const TodoItem: React.FC<TodoItemProps> = ({ item, onDelete }) => {
+const TodoItem: React.FC<TodoItemProps> = ({
+  item,
+  onDelete,
+  updateValue,
+  setUpdateValue,
+  handleUpdate
+}) => {
+  const [isShowUpdateInput, setIsShowUpdateInput] = useState(false)
   return (
     <div>
-      <input className="itembox" value={item} readOnly></input>
+      {isShowUpdateInput &&
+        <input type="text" value={updateValue}
+          onChange={(event) => setUpdateValue(event.target.value)}
+        />}
+      <input className="itembox" value={item} ></input>
       <button className="delbutton" onClick={onDelete}>
         삭제
       </button>
+      <button onClick={() => setIsShowUpdateInput(prev => !prev)} >
+        수정
+      </button>
+      {isShowUpdateInput && <button onClick={() => {
+        setIsShowUpdateInput(false)
+        handleUpdate()
+      }} >
+        확인
+      </button>}
     </div>
   );
 };
