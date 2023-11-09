20ea02558311b5aeb09d754fa0f3865a04fea011
기남후 <skagn4929@naver.com>
2023-11-09 18:22:30 +0900

    feat: 로컬스토리지 초기화 기능 추가

--- src/components/TodoBoard.tsx
+++ src/components/TodoBoard.tsx
@@ -8,57 +8,61 @@ type TodoBoardProps = {
 const TodoBoard: React.FC<TodoBoardProps> = ({ todoList }) => {
   const [todos, setTodos] = useState(todoList);
   const [inputValue, setInputValue] = useState("");
-  const [updateValue, setUpdateValue] = useState("")
-  const [showUpdateButtonTrigger, setShowUpdateButtonTrigger] = useState(false)
+  const [updateValue, setUpdateValue] = useState("");
   const handleAdd = () => {
     if (inputValue.trim() !== "") {
       setTodos([...todos, inputValue]);
       setInputValue("");
     }
-
   };
-  const handleDelete = (index: number) => { //1
+  const handleDelete = (index: number) => {
+    //1
     // #1
     // const newTodos = [...todos.slice(0, index), ...todos.slice(index + 1)];
     // setTodos(newTodos)
 
     // #2
-    const newTodos = todos.filter((_, innerIndex) => innerIndex !== index)
-    setTodos(newTodos)
+    const newTodos = todos.filter((_, innerIndex) => innerIndex !== index);
+    setTodos(newTodos);
 
     // #3
     // setTodos((prev) => prev.filter((_, innerIndex) => innerIndex !== index));
   };
 
-
-
   const handleUpdate = (index: number) => {
-    const newTodos = todos.map((todo, innerIndex) => { // 0,1,2    2 === 1
+    const newTodos = todos.map((todo, innerIndex) => {
+      // 0,1,2    2 === 1
       if (innerIndex === index) {
-        return updateValue
+        return updateValue;
       }
-      return todo
-    })
+      return todo;
+    });
 
     // console.log("newTodos: ", newTodos)
-    setTodos(newTodos)
-    setUpdateValue("")
-  }
+    setTodos(newTodos);
+    setUpdateValue("");
+  };
   const getLocalStorage = () => {
-    const todoList = localStorage.getItem("todoList")
+    const todoList = localStorage.getItem("todoList");
     if (todoList) {
-      setTodos(JSON.parse(todoList))
+      setTodos(JSON.parse(todoList));
     }
-  }
+  };
 
   const saveLocalStorage = () => {
-    localStorage.setItem("todoList", JSON.stringify(todos))
-  }
+    localStorage.setItem("todoList", JSON.stringify(todos));
+  };
+
+  const resetLocalStorage = () => {
+    localStorage.removeItem("todoList");
+    setTodos([]);
+  };
 
   return (
     <>
       <button onClick={saveLocalStorage}>로컬스토리지 저장</button>
       <button onClick={getLocalStorage}> 로컬스토리지 불러오기</button>
+      <button onClick={resetLocalStorage}> 로컬스토리지 초기화</button>
       <div className="todoboard">
         <input
           className="inputbox"
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -4,36 +4,43 @@ type TodoItemProps = {
   item: string;
   onDelete: () => void;
   handleUpdate: () => void;
-  updateValue: string
-  setUpdateValue: React.Dispatch<React.SetStateAction<string>>
+  updateValue: string;
+  setUpdateValue: React.Dispatch<React.SetStateAction<string>>;
 };
 const TodoItem: React.FC<TodoItemProps> = ({
   item,
   onDelete,
   updateValue,
   setUpdateValue,
-  handleUpdate
+  handleUpdate,
 }) => {
-  const [isShowUpdateInput, setIsShowUpdateInput] = useState(false)
+  const [isShowUpdateInput, setIsShowUpdateInput] = useState(false);
   return (
     <div>
-      {isShowUpdateInput &&
-        <input type="text" value={updateValue}
+      {isShowUpdateInput && (
+        <input
+          type="text"
+          value={updateValue}
           onChange={(event) => setUpdateValue(event.target.value)}
-        />}
-      <input className="itembox" value={item} ></input>
+        />
+      )}
+      <input className="itembox" value={item}></input>
       <button className="delbutton" onClick={onDelete}>
         삭제
       </button>
-      <button onClick={() => setIsShowUpdateInput(prev => !prev)} >
+      <button onClick={() => setIsShowUpdateInput((prev) => !prev)}>
         수정
       </button>
-      {isShowUpdateInput && <button onClick={() => {
-        setIsShowUpdateInput(false)
-        handleUpdate()
-      }} >
-        확인
-      </button>}
+      {isShowUpdateInput && (
+        <button
+          onClick={() => {
+            setIsShowUpdateInput(false);
+            handleUpdate();
+          }}
+        >
+          확인
+        </button>
+      )}
     </div>
   );
 };
